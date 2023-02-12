82a5e74fea49f8ebe5156749ef8f9ec9951b71f1
Your Name <you@example.com>
2023-02-12 09:57:43 +0000

    Added am new function to get employee Full Name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,30 @@
 fun main(args: Array<String>) {
         println("Payslip Printer")
+
+    val firstName= "Joe"
+    val surname= "Soap"
+    val gender = 'm'
+    val fullName = getFullName(firstName, surname, gender).toUpperCase()
+    println("$fullName")
         printPayslip()
         println("\n")
-        printPayslipWithRounding()
-    }
+        }
 
+//function returns the formatted string to two decimal places
+fun twoDecimalPlaces(value: Double): String {
+    return "%.2f".format(value)
+}
+
+//function takes in two parameters fName and sName and returns full name in the format of fName sName with uppercase
+fun getFullName(firstName: String, surname: String, gender: Char): String {
+    return if (gender == 'm') {
+        "Mr $firstName $surname"
+    } else if (gender == 'f') {
+        "Ms $firstName $surname"
+    } else {
+        "$firstName $surname"
+    }
+}
 fun printPayslip(){
     val firstName = "joe"
     val surname = "soap"
@@ -22,62 +42,24 @@ fun printPayslip(){
     val totalDeductions = payeAmount + prsiAmount + cycleToWorkDeduction
     val netSalary = monthlySalary + annualBonus - totalDeductions
 
-    println("| \t                           Employee Payslip                                                     |")
-    println("| ----------------------------------------------------------------------- |")
-    print("| Employee Name:  ${firstName.uppercase()} ${surname.uppercase()} ($gender)")
-    println(" \t Employee ID: $employeeId                        |")
-    println("| ----------------------------------------------------------------------- |")
-    print("| Payment Details ")
-    println(" Deduction Details                                      |")
-    println("| ----------------------------------------------------------------------- |")
-    print("| Gross Salary: $grossSalary \t ")
-    println(" PAYE: $payePercentage%                                 |")
-    print("| Annual Bonus: $annualBonus \t")
-    println("PRSI: $prsiPercentage%                                        |")
-    println("| \t\t\t\t\t\t Cycle to work: $cycleToWorkDeduction                             |")
-    println("| ----------------------------------------------------------------------- |")
-    println("| \t                        Net Salary: $netSalary                                           |")
-    println("| ----------------------------------------------------------------------- |")
+    val fullName = getFullName(firstName, surname, gender).toUpperCase()
 
-}
-
-fun printPayslipWithRounding(){
-    val firstName = "joe"
-    val surname = "soap"
-    val gender = 'm'
-    val employeeId = 6143
-    val grossSalary = 67543.21
-    val payePercentage = 38.5
-    val prsiPercentage = 5.2
-    val annualBonus = 1450.50
-    val cycleToWorkDeduction = 54.33
-
-    val monthlySalary = grossSalary / 12
-    val payeAmount = monthlySalary * payePercentage / 100
-    val prsiAmount = monthlySalary * prsiPercentage / 100
-    val totalDeductions = payeAmount + prsiAmount + cycleToWorkDeduction
-    val netSalary = monthlySalary + annualBonus - totalDeductions
-
-    //function returns the formatted string to two decimal places
-    fun formatAsTwoDecimalPlaces(value: Double): String {
-        return "%.2f".format(value)
-    }
 
     println("""
 | ----------------------------------------------------------------------- 
 |                           Employee Payslip                                                     
 | ----------------------------------------------------------------------- 
-| Employee Name:  ${firstName.uppercase()} ${surname.uppercase()} ($gender)                  Employee ID: $employeeId                        
+| Employee Name:  $fullName                      Employee ID: $employeeId                        
 | ----------------------------------------------------------------------- 
 | Payment Details                               Deduction Details                                      
 | ----------------------------------------------------------------------- 
-| Gross Salary: ${formatAsTwoDecimalPlaces(grossSalary)}                        PAYE: ${formatAsTwoDecimalPlaces(payePercentage)}%                                 
-| Annual Bonus: ${formatAsTwoDecimalPlaces(annualBonus)}                         PRSI: ${formatAsTwoDecimalPlaces(prsiAmount)}%                                        
-|                                               Cycle to work: ${formatAsTwoDecimalPlaces(cycleToWorkDeduction)}                             
+| Gross Salary: ${twoDecimalPlaces(grossSalary)}                        PAYE: ${twoDecimalPlaces(payePercentage)}%                                 
+| Annual Bonus: ${twoDecimalPlaces(annualBonus)}                         PRSI: ${twoDecimalPlaces(prsiAmount)}%                                        
+|                                               Cycle to work: ${twoDecimalPlaces(cycleToWorkDeduction)}                             
 | ----------------------------------------------------------------------- 
-| Total Deduction: ${formatAsTwoDecimalPlaces(totalDeductions)}                                                
+| Total Deduction: ${twoDecimalPlaces(totalDeductions)}                                                
 | ----------------------------------------------------------------------- 
-|                            Net Salary: ${formatAsTwoDecimalPlaces(netSalary)}                           
+|                            Net Salary: ${twoDecimalPlaces(netSalary)}                           
 | ----------------------------------------------------------------------- 
 """)}
 
